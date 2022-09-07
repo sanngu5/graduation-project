144857b8925f5f7d390b271b219d4cdc110b89b8
sanngu5 <ss7875@naver.com>
2022-09-08 01:57:48 +0900

    apply a tag in ActionButton

--- web/src/components/ActionButton.js
+++ web/src/components/ActionButton.js
@@ -1,43 +1,32 @@
-import React from 'react';
-import STATUS from '../constants/Status';
+import React from "react";
+import STATUS from "../constants/Status";
 
 const ActionButton = (props) => {
-    const {
-        selectedFile,
-        videoStatus,
-        onFileUpload,
-        onFileChanged,
-        onFileDownload
-    } = props;
+  const {
+    selectedFile,
+    videoStatus,
+    onFileUpload,
+    onFileChanged,
+    onFileDownload,
+  } = props;
 
-    if(videoStatus === STATUS["UPLOADABLE"]) {
-        return (
-            <button disabled={!selectedFile} onClick={onFileUpload}>
-                동영상 변환
-            </button>
-        )
-    } else if (videoStatus === STATUS["LOADING"]) {
-        return(
-            <button disabled={true} onClick={onFileChanged}>
-                변환중
-            </button>
-        )
-    } else if (videoStatus === STATUS["DOWNLOADABLE"]) {
-        return (
-            // <a href="http://127.0.0.1:5000/download">
-            //     다운로드
-            // </a>
-            <button 
-                // onClick={onFileDownload}
-                href="http://127.0.0.1:5000/download"
-                
-            >
-                다운로드
-            </button>
-        )
-    } else {
-        return (<></>)
-    }
-}
+  if (videoStatus === STATUS["UPLOADABLE"]) {
+    return (
+      <button disabled={!selectedFile} onClick={onFileUpload}>
+        동영상 변환
+      </button>
+    );
+  } else if (videoStatus === STATUS["LOADING"]) {
+    return (
+      <button disabled={true} onClick={onFileChanged}>
+        변환중
+      </button>
+    );
+  } else if (videoStatus === STATUS["DOWNLOADABLE"]) {
+    return <a href="http://127.0.0.1:5000/download">다운로드</a>;
+  } else {
+    return <></>;
+  }
+};
 
 export default ActionButton;
