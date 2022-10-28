2dc45f66ab7be05f7937c72aade4f547319aa3c2
truestar2001 <80587844+truestar2001@users.noreply.github.com>
2022-10-28 16:27:24 +0900

    Update test.py

--- ai/mask_extraction/test.py
+++ ai/mask_extraction/test.py
@@ -13,15 +13,18 @@ from model.network import UNet
 from torch.utils.data import DataLoader
 from thop import profile
 from thop import clever_format
+from model.deeplabV3 import DeepLab
 
-video_path='mask_extraction/test_imgs'
+video_path='../../../dataset/test/imgs/X/X0'
+save_path ='./test_mask/deeplab'
 n_frames=125
-mask_model_path='mask_extraction/checkpoint/MaskExtractor.pth'
+mask_model_path='./checkpoint/MaskExtractor2.pth'
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "4"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 if __name__ == '__main__':
-    masknet = UNet(n_channels=3, n_classes=1)
+    masknet = DeepLab(1, in_channels=3, backbone='resnet50', pretrained=True, 
+                output_stride=16, freeze_bn=False, freeze_backbone=False)
     masknet.load_state_dict(torch.load(mask_model_path))
     masknet=masknet.cuda()
     masknet=torch.nn.DataParallel(masknet,device_ids=[0])
@@ -33,11 +36,11 @@ if __name__ == '__main__':
         frames[i] = raw_frame
     frames = torch.from_numpy(np.transpose(frames, (0, 3, 1, 2)).copy()).float().cuda()
     frames=(frames-0.5)/0.5
-    frames=frames.unsqueeze(0)
+    #frames=frames.unsqueeze(0)
     with torch.no_grad():
         masks=masknet(frames).squeeze(0)
         masks = (masks > 0.5).float()
         for j in range(125):
             mask=masks[j]
             p_mask=transforms.ToPILImage()(mask).convert('RGB')
-            p_mask.save(os.path.join(video_path,'{:03d}.png'.format(j+1)))
+            p_mask.save(os.path.join(save_path,'{:03d}.png'.format(j+1)))
